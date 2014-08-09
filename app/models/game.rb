441436d7e677c5b535dffe03d6caaddda6a4178a
Flarnie Marchan <fnonemaker@gmail.com>
2014-08-09 08:51:16 -0700

    Add validations for attributes of Game and Life models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,3 @@
 class Game < ActiveRecord::Base
+  validates :name, uniqueness: true, presence: true, length: { minimum: 3, maximum: 81 }
 end
--- app/models/life.rb
+++ app/models/life.rb
@@ -1,2 +1,5 @@
 class Life < ActiveRecord::Base
+  # Make sure that there is only one lifeform on each tile in the game
+  validates :x_coord, uniqueness: { scope: [:y_coord, :game_id] }
+  validates :x_coord, :y_coord, :game_id, presence: true
 end
