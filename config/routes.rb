a0373d0e5710ce222c1ac04a4bef6ab326b52dab
Flarnie Marchan <fnonemaker@gmail.com>
2014-09-04 06:33:01 -0700

    Add first API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :games, :only => [:show, :index, :create, :edit, :update, :delete]
+  resources :games, only: [:show, :index, :create, :edit, :update, :delete]
+
+  namespace :api do
+    resources :games, only: [:show, :index]
+  end
 
   root 'games#new'
 end
