02a65d1442bdf3e3d48ca079104feab60c543fa6
Flarnie Marchan <fnonemaker@gmail.com>
2014-09-05 06:04:37 -0700

    Add local version of React so we can easily use React helper method "merge"

---
+++ app/assets/javascripts/dispatcher/app_dispatcher.js
@@ -0,0 +1,32 @@
+var Dispatcher = require('flux').Dispatcher,
+    merge = require('react/lib/merge');
+
+var AppDispatcher = merge(Dispatcher.prototype, {
+  /**
+   * Helper methods to wrap the dispatching of events from the server and views.
+   */
+
+  /*
+   * Handles data coming from the server
+   * @param {object} action
+   */
+  handleServerAction: function(action) {
+    this.dispatch({
+      source: 'SERVER_ACTION',
+      action: action
+    });
+  },
+
+  /*
+   * Handles data coming from the view
+   * @param {object} action
+   */
+  handleServerAction: function(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  },
+});
+
+module.exports = AppDispatcher;
