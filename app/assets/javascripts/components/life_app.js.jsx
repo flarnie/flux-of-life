4c0b7e2c2e9d9328c9cdf87f81bff919c09ace6c
Flarnie Marchan <fnonemaker@gmail.com>
2014-09-06 07:51:52 -0700

    Add LifeApp to contain EcosystemGrid for a given game
    Instead of rendering a 'show' page for each game and rendering the
    EcosystemGrid on the 'show' page, this will be a truly one page app.
    
    The LifeApp will have access to all games via the GamesStores, and will
    render a GamesIndex sidebar that can change the 'currentGame' in the
    GamesStores.

---
+++ app/assets/javascripts/components/life_app.js.jsx
@@ -0,0 +1,45 @@
+/** @jsx React.DOM */
+var EcosystemGrid = require('./ecosystem_grid.js.jsx'),
+    GameStores = require('../stores/game_stores'),
+    GameWebAPIUtils = require('../utils/game_web_api_utils'),
+    React = require('react');
+
+/**
+ * Because we are keeping records of all games in the stores, it will be
+ * easier to have a parent component that watches the stores and passes the
+ * right game to the ecosystemGrid.
+ * The ecosystemGrid is written as a controller component.
+ */
+var LifeApp = React.createClass({
+  /**
+  * @return {object}
+  */
+  getInitialState: function() {
+    return { games: GameStores.getAll() };
+  },
+
+  componentDidMount: function() {
+    GameStores.addChangeListener(this._onChange);
+    // Only now that we are listening for changes should we set off the call to
+    // fetch the games from the server.
+    GameWebAPIUtils.getAllGames();
+  },
+
+  compomentWillUnmount: function() {
+    GameStores.removeChangeListener(this._onChange);
+  },
+
+  _onChange: function() {
+    this.setState({ games: GameStores.getAll() });
+  },
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+    var currentGame = GameStores.find(this.props.gameId);
+    return (<EcosystemGrid game={currentGame} />);
+  }
+});
+
+module.exports = LifeApp;
