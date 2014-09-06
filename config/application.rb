fa3dd22a4c1db439d5b009f19c9cd9569461435f
Flarnie Marchan <fnonemaker@gmail.com>
2014-09-06 15:14:11 -0700

    Turn on React-Rails "addons" feature
    I want to use the ClassSet add-on feature of React, as well as other
    "addons". While changing the configuration, I fixed some indentation.

--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,17 @@ Bundler.require(*Rails.groups)
 module FluxOfLife
   class Application < Rails::Application
       config.generators do |g|
-    g.test_framework :rspec,
-      :fixtures => false,
-      :view_specs => false,
-      :helper_specs => false,
-      :routing_specs => false,
-      :controller_specs => true,
-      :request_specs => false
-    g.fixture_replacement :factory_girl, :dir => "spec/factories"
-  end
+        g.test_framework :rspec,
+          :fixtures => false,
+          :view_specs => false,
+          :helper_specs => false,
+          :routing_specs => false,
+          :controller_specs => true,
+          :request_specs => false
+        g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      end
 
+      config.react.addons = true;
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
