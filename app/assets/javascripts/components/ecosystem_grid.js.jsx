bfe3dee4a538c232a8ef6c8f7c664cc105848095
Flarnie Marchan <fnonemaker@gmail.com>
2014-09-07 08:34:57 -0700

    Add 'play' mode and 'edit' mode for EcosystemGrid
    Using the 'play' and 'edit' buttons, the mode of the grid can be
    toggled.  I may change this to a 'play' and 'pause' button in the future
    - it is implied that you can edit the grid when it is paused, because
      there are hover states then.
    
    The life steps aren't actually updating correctly at the moment when in
    'play' mode - fixing that is the next commit.

--- app/assets/javascripts/components/ecosystem_grid.js.jsx
+++ app/assets/javascripts/components/ecosystem_grid.js.jsx
@@ -1,7 +1,19 @@
 /** @jsx React.DOM */
-var GameStores = require('../stores/game_stores'),
+var $ = require('jquery'),
+    GameStores = require('../stores/game_stores'),
     React = require('react');
 
+/**
+ * constant {array} deltas
+ */
+const DELTAS = [[-1, 1],  [0, 1],  [1, 1],
+                [-1, 0],           [1, 0],
+                [-1, -1], [0, -1], [1, -1]];
+
+/**
+ * constant {number} life_step_length the time between steps in the Game of Life
+ */
+const LIFE_STEP_LENGTH = 2000; // 2 seconds
 /**
  * Expects state params of
  * game - a JSON representation of a Game model with nested life models.
@@ -9,17 +21,95 @@ var GameStores = require('../stores/game_stores'),
 var EcosystemGrid = React.createClass({
   getDefaultProps: function() {
     return {
-      size: 40
+      size: 40,
+      lifeStepLength: LIFE_STEP_LENGTH
     };
   },
 
   getInitialState: function() {
-    return { livesRecord: this._setLives() };
+    return {
+      livesRecord: this._setLives(this.props.game.lives),
+      playMode: false
+    };
   },
 
-  _setLives: function() {
+  _findNeighborLifeCount: function(x, y) {
+    var nCount = 0;
+    var neighbors = DELTAS.map((deltaCoords) => {
+        var newX = x + deltaCoords[0],
+            newY = y + deltaCoords[1];
+        return `${newX},${newY}`;
+      });
+    neighbors.forEach((neighborCoords) => {
+      if (this.state.livesRecord[neighborCoords]) {
+        nCount++;
+      }
+    });
+    return nCount;
+  },
+
+  /**
+   * Determines whether a cell lives to the next step or not.
+   * 1) Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+   * 2) Any live cell with two or three live neighbours lives on to the next generation.
+   * 3) Any live cell with more than three live neighbours dies, as if by overcrowding.
+   * 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+   * @param {object} options
+   * @return {boolean} whether it lives to the next step
+   */
+  _keepAlive: function(options) {
+    return (options.lifeCount === 3) || ((options.lifeCount === 2) && options.currentlyAlive);
+  },
+
+  _takeStep: function() {
+    var nextLifeCoords = [];
+    // for each of the coords in the grid
+    for (var y = 0; y < this.props.size; y++) {
+      for (var x = 0; x < this.props.size; x ++) {
+      // find it's neighbors
+      // count currently alive neighbors
+      var neighborLifeCount = this._findNeighborLifeCount(x, y);
+        // update this coord in the next life coords
+        var aliveAgain = this._keepAlive({
+          lifeCount: neighborLifeCount,
+          currentlyAlive: this.state.livesRecord[[x, y].join(',')]
+        });
+        if (aliveAgain) {
+          nextLifeCoords.push({ x_coord: x, y_coord: y });
+        }
+      }
+    }
+    // finally - set this.state.livesRecord to the next life coords
+    this._setLives(nextLifeCoords);
+    // trigger the next step after a delay
+    this._setNextStep();
+  },
+
+  _setNextStep: function() {
+    var timeoutId = window.setTimeout(this._takeStep, this.props.lifeStepLength);
+    this.props.timeoutId = timeoutId;
+    this.forceUpdate();
+  },
+
+  componentWillUnmount: function() {
+    this._stopPlayMode();
+  },
+
+  _stopPlayMode: function() {
+    if (this.props.timeOutId) {
+      window.clearTimeout(this.props.timeoutId);
+    }
+    this.setState({ playMode: false });
+  },
+
+  _startPlayMode: function() {
+    this.setState({ playMode: true });
+    this._setNextStep();
+  },
+
+  _setLives: function(nextLifeCoords) {
     var livesRecord = {};
-    this.props.game.lives.forEach(function (lifeCoords) {
+    nextLifeCoords.forEach(function (lifeCoords) {
       var x = lifeCoords.x_coord,
           y = lifeCoords.y_coord,
           coords = `${x},${y}`;
@@ -29,16 +119,34 @@ var EcosystemGrid = React.createClass({
     return livesRecord;
   },
 
+  _toggleLifeTile: function(coords) {
+    var updatedLivesRecord = this.state.livesRecord;
+    updatedLivesRecord[coords] = !updatedLivesRecord[coords];
+    this.setState({ livesRecord: updatedLivesRecord });
+  },
+
   _renderRow: function(y) {
     var tiles = [];
     for (var x = 0, innerLen = this.props.size; x < innerLen; x ++) {
-      var tileClasses = ['grid__tile'];
+      var tileClasses = { 'grid__tile': true };
       var coords = [x, y].join(',');
       if (this.state.livesRecord[coords]) {
-        tileClasses.push('grid__tile--alive');
+        tileClasses['grid__tile--alive'] = true;
+        if (this.state.playMode) {
+          tileClasses['grid__tile--in-play'] = true;
+        }
       }
+
+      var theTileClick = (this.state.playMode) ?
+        $.nooop : this._toggleLifeTile.bind(this, coords);
       var tileKey = String(x) +  '-' + String(y);
-      tiles.push((<div className={tileClasses.join(' ')} key={tileKey} />));
+      tiles.push((
+        <div
+          onClick={theTileClick}
+          className={React.addons.classSet(tileClasses)}
+          key={tileKey}
+        />
+      ));
     }
 
     return (
@@ -59,12 +167,32 @@ var EcosystemGrid = React.createClass({
     return rows;
   },
 
+  _saveGame: function() {
+    // TODO: update game
+  },
+
   render: function() {
-    this._setLives();
     return (
       <div className="ecosystem-grid">
         <div className="ecosystem-grid__title">
           <h2>{this.props.game.name}</h2>
+        </div>
+        <div className="ecosystem-grid__controls">
+          <button
+            disabled={this.state.playMode}
+            onClick={this._startPlayMode}>
+            Play
+          </button>
+          <button
+            disabled={!this.state.playMode}
+            onClick={this._stopPlayMode}>
+            Edit
+          </button>
+          <button
+            disabled={this.state.playMode}
+            onClick={this._saveGame}>
+            Save
+          </button>
         </div>
         <div className="ecosystem-grid__grid">
           {this._renderTiles()}
